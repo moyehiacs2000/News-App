bde8ac476452259620871f8480db05d271f601bb
Moyehiacs <mohamed2018170351@cis.asu.edu.eg>
2022-10-18 09:01:22 +0200

    adding splash screen

---
+++ News/app/src/main/java/com/moyehics/news/UI/SplashFragment.kt
@@ -0,0 +1,37 @@
+package com.moyehics.news.UI
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.moyehics.news.R
+import com.moyehics.news.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+    private var _binding:FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        binding.newslogo.animate().alpha(1.0f).setDuration(3000).start()
+        Handler(Looper.myLooper()!!).postDelayed({
+                                                 findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+          //  findNavController().popBackStack(R.id.splashFragment,true)
+        },3000)
+        val view = binding.root
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
